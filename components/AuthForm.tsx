274fe4e95973e50f2c188937963920dee36f113e
Hoacio Parisotto <parisottohoracio@gmail.com>
2024-07-23 12:32:58 -0300

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter(); 
@@ -47,8 +48,21 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
        // Sign up with Appwrite & create plain link token
 
+
 if(type === 'sign-up') {
-const newUser = await signUp(data);
+  const userData = {
+    firstName: data.firstName!,
+    lastName: data.lastName!,
+    address1: data.address1!,
+    city: data.city!,
+    state: data.state!,
+    postalCode: data.postalCode!,
+    dateOfBirth: data.dateOfBirth!,
+    ssn: data.ssn!,
+    email: data.email,
+    password: data.password,
+  }
+const newUser = await signUp(userData);
 
 setUser(newUser);
        }
@@ -98,12 +112,13 @@ setIsLoading(false);
 </h1>
 </div>
         </header>
-   {user ? (
+        {user ? (
     <div className="flex flex-col gap-4">
-{/* PlaidLink */}
+        <PlaidLink user={user} variant="primary" />
     </div>
-   ): (
-<>
+) : (
+    <div>
+       <>
 <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
        {type === 'sign-up' && (
@@ -170,10 +185,10 @@ label='Password' placeholder='Enter your password' />
             </Link>
           </footer>
 </>
+    </div>
+)}
 
-   )
-   
-   }
+{/* )} */}
     </section>
   )
 }
